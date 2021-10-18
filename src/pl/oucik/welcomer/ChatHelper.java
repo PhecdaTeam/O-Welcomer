c259c7314cb4287471a2f9ec9f5c7d06bd60e797
Out69 <53524461+Out69@users.noreply.github.com>
2021-10-18 20:51:40 +0200

    Delete src directory

--- src/pl/oucik/welcomer/ChatHelper.java
+++
@@ -1,9 +0,0 @@
-package pl.oucik.welcomer;
-import net.md_5.bungee.api.ChatColor;
-public class ChatHelper {
-    public static String color(String s) {
-        return ChatColor.translateAlternateColorCodes('&', s)
-                .replace(">>", "\u00BB")
-                .replace("<<", "\u00AB");
-    }
-}
